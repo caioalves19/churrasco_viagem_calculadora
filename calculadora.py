67ef653656ef4d59df78fe100faebf7d70d7cf89
Caio Alves <caioluizalves1995@gmail.com>
2022-10-08 12:44:47 -0300

    tirando comentarios

--- calculadora.py
+++ calculadora.py
@@ -169,31 +169,3 @@ class Produto:
 
     def __repr__(self) -> str:
         return self.nome
-
-# churrasco = Evento("Churrasco")
-# viagem = Evento("Viagem")
-
-# carne = Produto("Carne")
-# carvao = Produto("Carvão")
-
-# caio = Pessoa("Caio")
-# rafa = Pessoa("Rafael")
-# lezao = Pessoa("Lezão")
-
-
-# churrasco.add_pessoa(caio)
-# churrasco.add_pessoa(lezao)
-# churrasco.add_pessoa(rafa)
-
-# viagem.add_pessoa(caio)
-# viagem.add_pessoa(lezao)
-
-# caio.add_despesa(Despesa(carne, {caio: 30}), viagem)
-# caio.add_despesa(Despesa(carvao, {caio: 15, lezao: 10}), viagem)
-
-# caio.add_despesa(Despesa(carne, {caio: 30, lezao: 0}), churrasco)
-# caio.add_despesa(Despesa(carne, {caio: 100}), churrasco)
-# caio.add_despesa(Despesa(carvao, {caio: 0, lezao: 10, rafa: 0}), churrasco)
-
-# viagem.fechar_conta()
-# churrasco.fechar_conta()
